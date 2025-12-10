7c509316c55af33c845e6255cf4922aacf246df1
hoangkhoi_mobcec <hoangkhoi@bincorporation.com>
2025-12-10 08:52:48 +0700

    idk

--- apps/contact/next.config.js
+++ apps/contact/next.config.js
@@ -2,21 +2,16 @@
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
   assetPrefix: '/contact-static',
-  experimental: {
-    serverActions: {
-      allowedOrigins: [process.env.NEXT_PUBLIC_CONTACT_DOMAIN, process.env.NEXT_PUBLIC_HOME_DOMAIN]
-    }
-  },
   async rewrites() {
     return {
       beforeFiles: [
         {
           source: '/contact-static/_next/:path+',
-          destination: '/_next/:path+'
-        }
-      ]
-    }
-  }
-}
+          destination: '/_next/:path+',
+        },
+      ],
+    };
+  },
+};
 
-export default nextConfig
+export default nextConfig;
--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -1,29 +1,24 @@
-import '@repo/env-config'
+import '@repo/env-config';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
-  experimental: {
-    serverActions: {
-      allowedOrigins: [process.env.NEXT_PUBLIC_HOME_DOMAIN]
-    }
-  },
   async rewrites() {
     return [
       {
         source: '/contact',
-        destination: `${process.env.NEXT_PUBLIC_CONTACT_DOMAIN}/`
+        destination: `${process.env.NEXT_PUBLIC_CONTACT_DOMAIN}/`,
       },
       {
         source: '/contact/:path+',
-        destination: `${process.env.NEXT_PUBLIC_CONTACT_DOMAIN}/:path+`
+        destination: `${process.env.NEXT_PUBLIC_CONTACT_DOMAIN}/:path+`,
       },
       {
         source: '/contact-static/:path+',
-        destination: `${process.env.NEXT_PUBLIC_CONTACT_DOMAIN}/contact-static/:path+`
-      }
-    ]
-  }
-}
+        destination: `${process.env.NEXT_PUBLIC_CONTACT_DOMAIN}/contact-static/:path+`,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
